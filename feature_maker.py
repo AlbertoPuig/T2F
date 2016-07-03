615857f69dfad48bb1c2cc0d19a07f9545840c89
AlbertoPuig <alberto.puig.rguez@gmail.com>
2016-07-03 16:57:04 +0200

    added Gherkin labels

--- feature_maker.py
+++ feature_maker.py
@@ -14,6 +14,15 @@ def prepare(userstory):
 	parser.read('conf.ini')
 	feature = parser.get('gherkin_conf', 'feature')
 	scenario = parser.get('gherkin_conf','scenario')
+	language = parser.get('gherkin_conf','language')
+	size = parser.get('gherkin_conf','size')
+
+	freq = parser.get('gherkin_conf','freq')
+	scale = parser.get('gherkin_conf','scale')
+	ambit = parser.get('gherkin_conf','ambit')
+
+
+
 	given = parser.get('gherkin_conf', 'given')
 	other = parser.get('gherkin_conf', 'other')
 	path = parser.get('gherkin_conf', 'path')
@@ -35,6 +44,13 @@ def prepare(userstory):
 	#k = list(b.keys())
 	check_values = userstory.get_check()
 	file = open(filename + '.feature', "w")
+
+	file.write(language + "\n")
+	file.write(size + "\n")
+	file.write(freq + "\n")
+	file.write(scale + "\n")
+	file.write(ambit + "\n \n")
+
 	file.write(feature + ' ' + userstory.get_desc().replace('\n'," ") + " \n");
 	file.write(scenario + ' ' + userstory.get_name() + " \n")
 	
